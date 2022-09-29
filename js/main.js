4d626aedaed06bc4b68d9eabbabffdc77bd8a26b
Сергей <cirill2020@yandex.ru>
2022-09-29 21:01:43 +0700

    Добавил функциям условия

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 function getRandomNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  if (min < max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else return NaN;
+
 }
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 function getRandomFractionalNumber(min, max, fract) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return +((Math.floor(Math.random() * (max - min + 1)) + min) + Math.random()).toFixed(fract);
+  if (min < max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return +((Math.floor(Math.random() * (max - min + 1)) + min) + Math.random()).toFixed(fract);
+  } else return NaN;
 }
 
 getRandomNumber(1, 9);
