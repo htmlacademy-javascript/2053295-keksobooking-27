922efa66b218d0bd54f7a855414d1a08f33f4dda
Сергей <cirill2020@yandex.ru>
2022-10-01 17:55:46 +0700

    Переделал Math

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 const getRandomIntegerNumber = (min, max) => {
 
-  const lowerValue = Math.abs(Math.min(min, max));
-  const greaterValue = Math.abs(Math.max(min, max));
+  const lowerValue = Math.min(Math.abs(min), Math.abs(max));
+  const greaterValue = Math.max(Math.abs(min), Math.abs(max));
 
   return !isNaN(min, max) && min !== 0 && max !== 0 && min !== max ?
     Math.floor(Math.random() * (greaterValue - lowerValue + 1)) + lowerValue : NaN;
@@ -11,12 +11,11 @@ const getRandomIntegerNumber = (min, max) => {
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 const getRandomFractionalNumber = (min, max, fract) => {
 
-  const lowerValue = Math.abs(Math.min(min, max));
-  const greaterValue = Math.abs(Math.max(min, max));
+  const lowerValue = Math.min(Math.abs(min), Math.abs(max));
+  const greaterValue = Math.max(Math.abs(min), Math.abs(max));
 
   return !isNaN(min, max) && min !== 0 && max !== 0 && min !== max ?
-    (Math.random() * (greaterValue - lowerValue + 1)) + lowerValue.toFixed(fract) : NaN;
-
+    ((Math.random() * (greaterValue - lowerValue + 1)) + lowerValue).toFixed(fract) : NaN;
 };
 
 getRandomIntegerNumber(1, 9);
