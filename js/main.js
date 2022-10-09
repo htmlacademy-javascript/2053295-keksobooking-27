3fee73d22c56a3ffdde2a2d2103eb85dddc02af7
Сергей <cirill2020@yandex.ru>
2022-10-10 06:56:00 +0700

    Сделал массив с объектами

--- js/main.js
+++ js/main.js
@@ -1,55 +1,69 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 const getRandomIntegerNumber = (min, max) => {
-
   const lowerValue = Math.min(Math.abs(min), Math.abs(max));
   const greaterValue = Math.max(Math.abs(min), Math.abs(max));
-
-  return !isNaN(min, max) && min !== 0 && max !== 0 && min !== max ?
+  return !isNaN(min, max) && max !== 0 && min !== max ?
     Math.floor(Math.random() * (greaterValue - lowerValue + 1)) + lowerValue : NaN;
 };
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 const getRandomFractionalNumber = (min, max, fract) => {
-
   const lowerValue = Math.min(Math.abs(min), Math.abs(max));
   const greaterValue = Math.max(Math.abs(min), Math.abs(max));
-
-  return !isNaN(min, max) && min !== 0 && max !== 0 && min !== max ?
+  return !isNaN(min, max) && max !== 0 && min !== max ?
     ((Math.random() * (greaterValue - lowerValue + 1)) + lowerValue).toFixed(fract) : NaN;
 };
+// getRandomIntegerNumber(1, 9);
+// getRandomFractionalNumber(1.5, 3.5, 2);
+const PICTURES = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+];
+const MIN_PRICE = 1000;
+const MAX_PRICE = 100000;
+const LAT_MIN = 35.65;
+const LAT_MAX = 35.70;
+const LNG_MIN = 139.70;
+const LNG_MAX = 139.80;
+const ARRAY_LENGTH = 10;
+const type = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkin = ['12:00', '13:00', '14:00'];
+const checkout = ['12:00', '13:00', '14:00'];
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
 
-getRandomIntegerNumber(1, 9);
-getRandomFractionalNumber(1, 3, 2);
+const createAuthor = () => {
+  const randomPictureIndex = getRandomIntegerNumber(0, PICTURES.length - 1);
+  return randomPictureIndex >= 9 ? { avatar: `img/avatars/user${ PICTURES[randomPictureIndex] }.png` } : { avatar: 'img/avatars/user' + `0${ PICTURES[randomPictureIndex] }.png` };
+};
 
-// author, объект — описывает автора. Содержит одно поле:
+const createOffer = () => ({
+  title: 'Заголовок',
+  address: '',
+  price: getRandomIntegerNumber(MIN_PRICE, MAX_PRICE),
+  type: type[getRandomIntegerNumber(0, type.length - 1)],
+  rooms: getRandomIntegerNumber(1, 10),
+  guests: getRandomIntegerNumber(1, 10),
+  checkin: checkin[getRandomIntegerNumber(0, checkin.length - 1)],
+  checkout: checkout[getRandomIntegerNumber(0, checkout.length - 1)],
+  features: features.slice(getRandomIntegerNumber(0, features.length - 1)),
+  description: 'Какоето описание',
+  photos: photos.slice(getRandomIntegerNumber(0, photos.length - 1)),
+});
 
-// avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10. Перед однозначными числами ставится 0. Например, 01, 02...10. Адреса изображений не повторяются.
-// offer, объект — содержит информацию об объявлении. Состоит из полей:
+const createLocation = () => ({
+  lat: getRandomFractionalNumber(LAT_MIN, LAT_MAX, 5),
+  lng: getRandomFractionalNumber(LNG_MIN, LNG_MAX, 5),
+});
 
-// title, строка — заголовок предложения. Придумайте самостоятельно.
+const createObject = () => ({
+  author: createAuthor(),
+  offer: createOffer(),
+  location: createLocation(),
+});
 
-// address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.lat}}, {{location.lng}}.
+const similarObjects = Array.from({ length: ARRAY_LENGTH}, createObject);
 
-// price, число — стоимость. Случайное целое положительное число.
-
-// type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
-
-// rooms, число — количество комнат. Случайное целое положительное число.
-
-// guests, число — количество гостей, которое можно разместить. Случайное целое положительное число.
-
-// checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-
-// checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-
-// features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
-
-// description, строка — описание помещения. Придумайте самостоятельно.
-
-// photos, массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
-
-// location, объект — местоположение в виде географических координат. Состоит из двух полей:
-
-// lat, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
-
-// lng, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000.
+console.log(similarObjects);
