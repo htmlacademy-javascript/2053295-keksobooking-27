b753f9bcf9b6e9a40e7d8d000b8f51b66ab7da8f
Сергей <cirill2020@yandex.ru>
2022-10-13 13:15:12 +0700

    Переделал address и avatar

--- js/main.js
+++ js/main.js
@@ -13,55 +13,48 @@ const getRandomFractionalNumber = (min, max, fract) => {
   return !isNaN(min, max) && max !== 0 && min !== max ?
     ((Math.random() * (greaterValue - lowerValue)) + lowerValue).toFixed(fract) : NaN;
 };
-// getRandomIntegerNumber(1, 9);
-// getRandomFractionalNumber(1.5, 3.5, 2);
-const PICTURES = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-];
-const MIN_PRICE = 1000;
-const MAX_PRICE = 100000;
-const LAT_MIN = 35.65000;
-const LAT_MAX = 35.70000;
-const LNG_MIN = 139.70000;
-const LNG_MAX = 139.80000;
-const ARRAY_LENGTH = 10;
-const type = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const checkin = ['12:00', '13:00', '14:00'];
-const checkout = ['12:00', '13:00', '14:00'];
-const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const MIN_PRICE = 1000; //Минимальная цена
+const MAX_PRICE = 100000; //Максимальная цена
+const LAT_MIN = 35.65000; //Координаты. Минимальная широта
+const LAT_MAX = 35.70000; //Координаты. Максимальная широта
+const LNG_MIN = 139.70000; //Координаты. Минимальная долгота
+const LNG_MAX = 139.80000; //Координаты. Максимальная долгота
+const ARRAY_LENGTH = 10; //Длина массива с объектами
+const types = ['palace', 'flat', 'house', 'bungalow', 'hotel']; //Тип жилья
+const checkinTimes = ['12:00', '13:00', '14:00']; //Время заезда
+const checkoutTimes = ['12:00', '13:00', '14:00']; //Время выезда
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']; //Доп. опции
 const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg', //Фотографии
 ];
-
-const createAuthor = () => {
-  const randomPictureIndex = getRandomIntegerNumber(0, PICTURES.length - 1);
-  return randomPictureIndex >= 9 ? { avatar: `img/avatars/user${ PICTURES[randomPictureIndex] }.png` } : { avatar: `img/avatars/user0${ PICTURES[randomPictureIndex] }.png` };
-};
-
-const createOffer = () => ({
-  title: 'Заголовок',
-  address: location.lat + location.lng,
-  price: getRandomIntegerNumber(MIN_PRICE, MAX_PRICE),
-  type: type[getRandomIntegerNumber(0, type.length - 1)],
-  rooms: getRandomIntegerNumber(1, 10),
-  guests: getRandomIntegerNumber(1, 10),
-  checkin: checkin[getRandomIntegerNumber(0, checkin.length - 1)],
-  checkout: checkout[getRandomIntegerNumber(0, checkout.length - 1)],
-  features: features.slice(getRandomIntegerNumber(0, features.length - 1)),
-  description: 'Какоето описание',
-  photos: photos.slice(getRandomIntegerNumber(0, photos.length - 1)),
-});
-
+let pictureIndex = 0; //Индекс аватарки
 const createLocation = () => ({
   lat: getRandomFractionalNumber(LAT_MIN, LAT_MAX, 5),
   lng: getRandomFractionalNumber(LNG_MIN, LNG_MAX, 5),
 });
-
+const coordinates = createLocation();
+const createAuthor = () => {
+  pictureIndex += 1;
+  return { avatar: `img/avatars/user${pictureIndex.toString().padStart(2, '0')}.png` };
+};
+const createOffer = () => ({
+  title: 'Заголовок',
+  address: [coordinates.lat, coordinates.lng].join('--'),
+  price: getRandomIntegerNumber(MIN_PRICE, MAX_PRICE),
+  type: types[getRandomIntegerNumber(0, types.length - 1)],
+  rooms: getRandomIntegerNumber(),
+  guests: getRandomIntegerNumber(),
+  checkin: checkinTimes[getRandomIntegerNumber(0, checkinTimes.length - 1)],
+  checkout: checkoutTimes[getRandomIntegerNumber(0, checkoutTimes.length - 1)],
+  features: features.slice(getRandomIntegerNumber(0, features.length - 1)),
+  description: 'Какоето описание',
+  photos: photos.slice(getRandomIntegerNumber(0, photos.length - 1)),
+});
 const createObject = () => ({
   author: createAuthor(),
   offer: createOffer(),
-  location: createLocation(),
+  location: coordinates,
 });
-
-const similarObjects = Array.from({ length: ARRAY_LENGTH }, createObject);
+const similarObjects = () => Array.from({ length: ARRAY_LENGTH }, createObject);
+similarObjects();
