616dbf473f4ed1530414c55b977a06719aa404cf
Сергей <cirill2020@yandex.ru>
2022-11-04 16:19:56 +0700

    const types = ['palace', 'flat', 'house', 'bungalow', 'hotel']; //Тип жилья

---
+++ js/similar-items.js
@@ -0,0 +1,120 @@
+import {
+  HouseType
+} from './constants.js';
+
+const renderSimilarCards = (cards) => {
+  const fragment = document.createDocumentFragment();
+  const mapCanvas = document.querySelector('#map-canvas');
+
+  cards.forEach((item) => {
+    const templateFragment = document.querySelector('#card').content.querySelector('.popup');
+    const cardItem = templateFragment.cloneNode(true);
+    // const featuresContainer = cardItem.querySelector('.popup__features');
+    const featuresList = cardItem.querySelector('.popup__features').querySelectorAll('.popup__feature');
+    const photoContainer = cardItem.querySelector('.popup__photos');
+    const cardItemAvatar = cardItem.querySelector('.popup__avatar');
+    const cardItemTitle = cardItem.querySelector('.popup__title');
+    const cardItemAddress = cardItem.querySelector('.popup__text--address');
+    const cardItemPrice = cardItem.querySelector('.popup__text--price');
+    const cardItemType = cardItem.querySelector('.popup__type');
+    const cardItemCapacity = cardItem.querySelector('.popup__text--capacity');
+    const cardItemTime = cardItem.querySelector('.popup__text--time');
+    const cardItemDescription = cardItem.querySelector('.popup__description');
+
+    const { avatar, } = item.author;
+    const { lat, lng, } = item.location;
+    const { address, checkin, checkout, description, features, guests, photos, price, rooms, title, type, } = item.offer;
+
+    let roomsWord;
+    let guestsWord;
+
+    if (rooms === 1) {
+      roomsWord = 'комната';
+    } else if (rooms >= 5) {
+      roomsWord = 'комнат';
+    } else {
+      roomsWord = 'комнаты';
+    }
+    if (guests === 1) {
+      guestsWord = 'гостя';
+    } else {
+      guestsWord = 'гостей';
+    }
+
+    if (!avatar) {
+      cardItemAvatar.remove();
+    } else {
+      cardItemAvatar.src = avatar;
+    }
+
+    if (!title) {
+      cardItemTitle.remove();
+    } else {
+      cardItemTitle.textContent = title;
+    }
+
+    if (!lat || !lng) {
+      cardItemAddress.remove();
+    } else {
+      cardItemAddress.textContent = address;
+    }
+
+    if (!price) {
+      cardItemPrice.remove();
+    } else {
+      cardItemPrice.textContent = `${price} ₽/ночь`;
+    }
+
+    if (!type) {
+      cardItemType.remove();
+    } else {
+      cardItemType.textContent = HouseType[type];
+    }
+
+    if (!rooms || !guests) {
+      cardItemCapacity.remove();
+    } else {
+      cardItemCapacity.textContent = `${rooms} ${roomsWord} для ${guests} ${guestsWord}`;
+    }
+
+    if (!checkin && !checkout) {
+      cardItemTime.textContent = 'Заезд после --:-- , выезд до --:--';
+    } else if (!checkin) {
+      cardItemTime.textContent = `Выезд до ${checkout}`;
+    } else if (!checkout) {
+      cardItemTime.textContent = `Заезд после ${checkin}`;
+    } else {
+      cardItemTime.textContent = `Заезд после ${checkin} , выезд до ${checkout}`;
+    }
+
+    if (!description) {
+      cardItemDescription.remove();
+    } else {
+      cardItemDescription.textContent = description;
+    }
+
+    featuresList.forEach((featuresItem) => {
+      const receivedFeatures = (featuresItems) =>
+        featuresItem.classList.contains(`popup__feature--${featuresItems}`);
+      if (!features.some(receivedFeatures)) {
+        featuresItem.remove();
+      }
+    });
+
+    photos.forEach((photo) => {
+      const templatePhotos = document.querySelector('#housing-photos').content.querySelector('.popup__photo');
+      if (photo) {
+        const photoItem = templatePhotos.cloneNode(true);
+        photoItem.src = photo;
+        photoContainer.appendChild(photoItem);
+      }
+    });
+    fragment.appendChild(cardItem);
+  });
+
+  mapCanvas.appendChild(fragment);
+};
+
+export {
+  renderSimilarCards
+};
---
+++ js/util.js
@@ -0,0 +1,19 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
+const getRandomIntegerNumber = (min, max) => {
+  const lowerValue = Math.min(Math.abs(min), Math.abs(max));
+  const greaterValue = Math.max(Math.abs(min), Math.abs(max));
+  return !isNaN(min, max) && max !== 0 && min !== max ?
+    Math.floor(Math.random() * (greaterValue - lowerValue + 1)) + lowerValue : NaN;
+};
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+const getRandomFractionalNumber = (min, max, fract) => {
+  const lowerValue = Math.min(Math.abs(min), Math.abs(max));
+  const greaterValue = Math.max(Math.abs(min), Math.abs(max));
+  return !isNaN(min, max) && max !== 0 && min !== max ?
+    ((Math.random() * (greaterValue - lowerValue)) + lowerValue).toFixed(fract) : NaN;
+};
+
+export {
+  getRandomIntegerNumber,
+  getRandomFractionalNumber,
+};
